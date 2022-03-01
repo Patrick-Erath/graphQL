4a44ae741168b9863b7ff8250ca5ef3ed9b580c5
Patrick-Erath <41576603+Patrick-Erath@users.noreply.github.com>
2022-03-01 15:33:17 -0500

    Added mock data types

--- erp-database-app/initial-project/index.js
+++ erp-database-app/initial-project/index.js
@@ -1,14 +1,84 @@
 const { ApolloServer } = require("apollo-server");
+const { gql } = require('apollo-server')
 
-exports.typeDefs = gql`
+let typeDefs = gql`
   type Query {
     cars: [Car!]!
+  }
+
+  type Mutation {
+    groupDelete(groupId: ID)
+    groupPublish(groupId: ID)
+    groupUnpublish(groupId: ID)
+    groupAddCars(groupId: ID!, carID: ID!)
+    groupRemoveCars(groupId: ID!, carID: ID!)
+    groupCreate(
+      groupInput: GroupInput!
+    ): Group
+    groupUpdate(
+      groupId: ID!
+      input: GroupInput!
+    ): GroupUpdatePayload!
+  }
+
+  type GroupUpdatePayload {
+    userErrors: [UserErrors!]!
+    group: Group
+  }
+
+  type UserErrors { 
+    message: String!
+    field: [String!]!
+  }
+
+  input GroupInput {
+    name: String
+      name: String
+      image: ImageInput
+      description: String
+      featureSet: GroupFeatureFields
+  }
+
+  input ImageInput {
+    url: String!
   }
 
   type Car {
     id: ID!
     color: String!
     make: String!
+  }
+
+  type Group {
+    id: ID!
+    featureSet: GroupFeatureSet
+    hasCar(id: ID!): Boolean!
+    cars(skip: Int!, take: Int!): [Car!]!
+    name: String!
+    imageId: Image!
+    description: String!
+  }
+
+  type Image {
+    id: ID!
+    url: String!
+  }
+
+  type GroupFeatureSet {
+    features: [GroupFeatures!]!
+    applyFeaturesSeperately: Boolean!
+  }
+
+  type GroupFeatures {
+    feature: GroupFeatureFields!
+  }
+
+  enum GroupFeatureFields {
+    INCLINE_ENGINE
+    FOUR_CYLINDER_ENGINE
+    TWIN_CYLINDER_ENGINE
+    RED_PAINT
+    BLACK_PAINT
   }
 `;
 
